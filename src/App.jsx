c0524b3665326732f2e52657868aa65a0643d0f7
YoussufSalah <youssufsalahsaber@gmail.com>
2025-06-22 06:44:17 +0300

    Test: Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import Hero from './components/Hero';
-import Features from './components/Features';
-import CTA from './components/CTA';
-import Footer from './components/Footer';
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import CTA from "./components/CTA";
+import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
-  return (
-    <div>
-      <Hero />
-      <Features />
-      <CTA />
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <Hero />
+            <Features />
+            <CTA />
+            <Footer />
+            <Analytics />
+        </div>
+    );
 }
 
 export default App;
